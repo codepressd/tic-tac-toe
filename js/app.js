95191d6a7e5d23d83cbb8e8f0e2a354e4b255fcf
Codepressd <creederone@gmail.com>
2016-08-03 13:05:53 -0700

    the X's and O's are inputed and switch per click

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
-	
+$(function(){
+	var TicTacToe = {
+
+		varSwitch : function(str){
+
+			if(str === ''){
+				return 'x';
+			}else if(str === 'x'){
+				return 'o';
+			}else{
+				return 'x';
+			}
+
+		}
+
+	}
+
+ 
+
+
+var ohhAndEx = '';
+
+	$('.game-grid').on('click','td',function(event){
+		
+		ohhAndEx = TicTacToe.varSwitch(ohhAndEx);
+		var $target = $(event.target);
+
+		$($target).html('<h2>'+ohhAndEx+'</h2>');
+
+
+	});
+});
+
+//var $target = $(event.target);
+//		var index = $target.index();	
