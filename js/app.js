6dc4e57351477b5b57784a7a49d17881e1373bf3
Codepressd <creederone@gmail.com>
2016-08-04 11:37:32 -0700

    compare function is almost finished but can't seem to break the for loop

--- js/app.js
+++ js/app.js
@@ -1,22 +1,50 @@
 	var TicTacToe = {
 
-		playerInput:[[],[],[]],
+		playerInput:[["","",""],["","",""],["","",""]],
 
 		varSwitch : function(str){
 
-			if(str === ''){
+			if(str === 'o'){
 				return 'x';
-			}else if(str === 'x'){
-				return 'o';
 			}else{
-				return 'x';
+				return 'o';
 			}
 
 		},
 
 		addInput : function(pIndex,tarIndex,tarValue){
 
-			TicTacToe.playerInput[pIndex].splice(tarIndex, 0, tarValue);
+			TicTacToe.playerInput[pIndex].splice(tarIndex, 1, tarValue);
+
+		},
+
+
+		compareFunc : function(){
+			//compare all rows
+			
+			if(TicTacToe.playerInput[0][0] === TicTacToe.playerInput[1][1] && TicTacToe.playerInput[0][0] === TicTacToe.playerInput[2][2]){
+				alert(TicTacToe.playerInput[0][0] + ' Won The Game!');
+				
+			}else if(TicTacToe.playerInput[2][0] === TicTacToe.playerInput[1][1] && TicTacToe.playerInput[2][0] === TicTacToe.playerInput[0][2]){
+				alert(TicTacToe.playerInput[2][0] + ' Won The Game!');
+				
+			}else{
+
+				for(var i =0; i < TicTacToe.playerInput.length; i++ ){
+					
+					 if(TicTacToe.playerInput[i][0] === TicTacToe.playerInput[i][1] && TicTacToe.playerInput[i][0] === TicTacToe.playerInput[i][2]){
+						alert(TicTacToe.playerInput[i][0] + ' Won The Game!');
+						
+					}
+					for(var j =0; j < TicTacToe.playerInput[i].length; j++){
+						if(TicTacToe.playerInput[0][j] == TicTacToe.playerInput[1][j] && TicTacToe.playerInput[0][j] == TicTacToe.playerInput[2][j]){
+						alert(TicTacToe.playerInput[0][j] + ' Won The Game!');
+						
+						}
+					}
+					
+				}
+			}//end of else
 
 		}
 
@@ -28,18 +56,23 @@ $(function(){
  
 
 
-var ohhAndEx = '';
+var ohhAndEx = 'o';
 
 	$('.game-grid').on('click','td',function(event){
 		
 		ohhAndEx = TicTacToe.varSwitch(ohhAndEx);
 		var $target = $(event.target);
-		TicTacToe.addInput($target.parent().index(),$target.index(),ohhAndEx);
-		$($target).html('<h2>'+ohhAndEx+'</h2>');
+
+		if($target.text() == ''){
+			TicTacToe.addInput($target.parent().index(),$target.index(),ohhAndEx);
+			$target.html('<h2>'+ohhAndEx+'</h2>');
+		}else{
+			alert("You can't do that! Space Already Taken");
+		}
+		
 
 
 	});
 });
 
-//var $target = $(event.target);
-//		var index = $target.index();	
+	
