a64056920fe084682d5a17fd94eab0524fdfe819
Codepressd <creederone@gmail.com>
2016-08-04 13:00:15 -0700

    finished the game so it reloads after someone wins

--- js/app.js
+++ js/app.js
@@ -4,10 +4,10 @@
 
 		varSwitch : function(str){
 
-			if(str === 'o'){
-				return 'x';
+			if(str === 'O'){
+				return 'X';
 			}else{
-				return 'o';
+				return 'O';
 			}
 
 		},
@@ -18,35 +18,61 @@
 
 		},
 
+		winTeam : '',
+
 
 		compareFunc : function(){
 			//compare all rows
+			var userWon = false;
 			
 			if(TicTacToe.playerInput[0][0] === TicTacToe.playerInput[1][1] && TicTacToe.playerInput[0][0] === TicTacToe.playerInput[2][2]){
-				alert(TicTacToe.playerInput[0][0] + ' Won The Game!');
+					TicTacToe.winTeam = TicTacToe.playerInput[0][0];
+					return true;
+				
 				
 			}else if(TicTacToe.playerInput[2][0] === TicTacToe.playerInput[1][1] && TicTacToe.playerInput[2][0] === TicTacToe.playerInput[0][2]){
-				alert(TicTacToe.playerInput[2][0] + ' Won The Game!');
+					TicTacToe.winTeam = TicTacToe.playerInput[2][0];
+					return true;
 				
 			}else{
 
 				for(var i =0; i < TicTacToe.playerInput.length; i++ ){
 					
 					 if(TicTacToe.playerInput[i][0] === TicTacToe.playerInput[i][1] && TicTacToe.playerInput[i][0] === TicTacToe.playerInput[i][2]){
-						alert(TicTacToe.playerInput[i][0] + ' Won The Game!');
+						TicTacToe.winTeam = TicTacToe.playerInput[i][0];	
+						return true;
 						
 					}
 					for(var j =0; j < TicTacToe.playerInput[i].length; j++){
 						if(TicTacToe.playerInput[0][j] == TicTacToe.playerInput[1][j] && TicTacToe.playerInput[0][j] == TicTacToe.playerInput[2][j]){
-						alert(TicTacToe.playerInput[0][j] + ' Won The Game!');
-						
+							if(userWon){
+								break;
+							}else{
+								TicTacToe.winTeam = TicTacToe.playerInput[0][j];
+								userWon = true;
+								return true;
+							}
 						}
 					}
 					
 				}
 			}//end of else
 
-		}
+		},
+
+		gameReset : function(){
+
+			window.location.reload();
+		},
+
+		/*emptyArray: function(){
+
+			for(var i =0; i<TicTacToe.playerInput.length; i++){
+				for(var j=0; j<TicTacToe.playerInput[i].length; j++){
+					TicTacToe.playerInput[i][j] = "";
+				}
+			}
+		}*/
 
 	};
 
@@ -56,7 +82,7 @@ $(function(){
  
 
 
-var ohhAndEx = 'o';
+var ohhAndEx = 'O';
 
 	$('.game-grid').on('click','td',function(event){
 		
@@ -66,6 +92,12 @@ var ohhAndEx = 'o';
 		if($target.text() == ''){
 			TicTacToe.addInput($target.parent().index(),$target.index(),ohhAndEx);
 			$target.html('<h2>'+ohhAndEx+'</h2>');
+			TicTacToe.compareFunc();
+			if(TicTacToe.winTeam !== ''){
+				$('.lightbox').css('display','block');
+				$('.lightbox > h1').text(TicTacToe.winTeam +' Team Won The Game');
+				setTimeout(function(){$('.lightbox').css('display','none'); TicTacToe.gameReset();}, 2000);
+			}
 		}else{
 			alert("You can't do that! Space Already Taken");
 		}
